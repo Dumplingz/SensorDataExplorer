eb66f020d415b6531d15d74429836e9e240045d1
mkapila779 <mkapila779@fhs.win.fuhsd.org>
2016-11-02 10:48:51 -0700

    added new method to find next SD

--- src/Sectioning.java
+++ src/Sectioning.java
@@ -1,11 +1,17 @@
 
 public class Sectioning {
 	/***
-	 * Take in data
+	 * parem@ Take in data double arr[]
 	 * Identify two SD (min and max)
 	 */
 	public static void SDofParts(){
 
+		
+	}
+	/***
+	 * If next few peaks are less then SD min and max, create new SD min ans max of the few peaks  
+	 */
+	public static void NextSD(){
 		
 	}
 }
