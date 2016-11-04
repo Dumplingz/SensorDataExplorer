5c35ca179a2c4f04a275ebe6ae5859c596f8b57b
mkapila779 <mkapila779@fhs.win.fuhsd.org>
2016-11-04 11:03:20 -0700

    separates data into n sections to find its threshold

--- src/Sectioning.java
+++ src/Sectioning.java
@@ -3,13 +3,13 @@ import java.util.List;
 
 public class Sectioning {
 
-
 	/***
 	 * Create new double array with peaks
+	 * 
 	 * @param sensorData
 	 * @return array with identified peaks
 	 */
-	public static double[] IdentifyPeaks(double[][] sensorData){
+	public static double[] IdentifyPeaks(double[][] sensorData) {
 		ArrayList<Double> timeOfPeaks = new ArrayList<Double>();
 		double[] magnitudes = StepCounter.calculateMagnitudesFor(sensorData);
 
@@ -24,8 +24,8 @@ public class Sectioning {
 			}
 		}
 		double[] peakValues = new double[peaks];
-		
-		for (int i = 0; i < timeOfPeaks.size() ; i++) {
+
+		for (int i = 0; i < timeOfPeaks.size(); i++) {
 			peakValues[i] = timeOfPeaks.get(i);
 		}
 		return peakValues;
@@ -33,22 +33,68 @@ public class Sectioning {
 
 	public static double calculateThresholdMax(double[] arr) {
 		double mean = StepCounter.calculateMean(arr);
-		return mean + (StepCounter.calculateStandardDeviation(arr, mean)*2);
+		return mean + (StepCounter.calculateStandardDeviation(arr, mean) * 2);
 
 	}
 
 	/***
-	 * parem@ Take in data double arr[]
+	 * @param Take in data double arr[][]
 	 * Identify two SD (min and max)
 	 */
-	public static void SDofParts(){
+	public static double[] TakeofPartsData(double[][] sensorData, int n){
+		double[] arr = IdentifyPeaks(sensorData);
+		double maxThreshold = calculateThresholdMax(arr);
+		for(int j = 0; j < arr.length; j++){
+		for(int i = 0; i < (int)(n/2); i++){
+			double Threshold = StepCounter.calculateThreshold(arr);	
+			if(Threshold > arr[i+1] && maxThreshold < arr[i+1] && Threshold > arr[i] && maxThreshold < arr[i] && Threshold > arr[((int)(n/2))-i] && maxThreshold < arr[((int)(n/2))-i]){
+				double[] NewSection = new double[n];
+				NewSection[i] = 
+				return NewSection;
+			}
+		}
+		}
+ 		return null;
+	}
 
+	public static double[] getStillSections(){
+		double leftvalue;
+		leftvalue = rightvalue(peaksection)
 		
 	}
-	/***
-	 * If next few peaks are less then SD min and max, create new SD min ans max of the few peaks  
+
+	/**
+	 * 
+	 * 
+	 * @param sensorData
+	 * @param n
+	 * @return
 	 */
-	public static void NextSD(){
-		
+	public static double[] NSectionsByThresholds(double[][] sensorData, int n) {
+		/**
+		 * TODO delete 3 (int this method n = 3 FIX IT) section data into n
+		 * parts. Identify threshold per section and save different thresholds
+		 */
+		double[] arr = IdentifyPeaks(sensorData);
+		double[] ThresholdByN = new double[(int)(arr.length/3)];
+		int j = 0;
+		for (int i = 0; i < arr.length - 3; i = i + 3) {
+			double[] newArr = new double[3];
+			newArr[0] = arr[i];
+			newArr[1] = arr[i + 1];
+			newArr[2] = arr[i + 2];
+			double Threshold = StepCounter.calculateThreshold(newArr);
+			ThresholdByN[j] = Threshold;
+			j++;
+		}
+		return newArr;
+	}
+
+	/***
+	 * If next few peaks are less then SD min and max, create new SD min ans max
+	 * of the few peaks
+	 */
+	public static void NextSD() {
+
 	}
 }
